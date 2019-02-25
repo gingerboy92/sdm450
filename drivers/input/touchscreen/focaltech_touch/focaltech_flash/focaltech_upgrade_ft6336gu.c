f55e1cd78fe08d10b2c05af8328d43ebb9a57258
LinuxPanda <LinuxPanda@users.noreply.github.com>
2019-02-25 16:24:21 +0530

    fix focaltech touchscreen compile failure
    format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘size_t’ [-Wformat]
    
    fix from, https://stackoverflow.com/a/12875589

--- drivers/input/touchscreen/focaltech_touch/focaltech_flash/focaltech_upgrade_ft6336gu.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_flash/focaltech_upgrade_ft6336gu.c
@@ -234,7 +234,7 @@ static int fts_ft6x36gu_get_app_bin_file_ver(struct i2c_client *client,
 	}
 
 	if (fw->size < APP_FILE_MIN_SIZE || fw->size > APP_FILE_MAX_SIZE)
-		FTS_ERROR("[UPGRADE]: FW length(%x) error", fw->size);
+		FTS_ERROR("[UPGRADE]: FW length(%zu) error", fw->size);
 	else
 		fw_ver = fw->data[APP_FILE_VER_MAPPING];
 
