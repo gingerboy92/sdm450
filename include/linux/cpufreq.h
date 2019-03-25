1aef6dab779fdbc73df85278f53a205fa5974acf
Dimitar Yurukov <mscalindt@protonmail.com>
2019-03-25 14:22:13 +0200

    drivers: cpufreq: Yankactive governor
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -507,6 +507,9 @@ extern struct cpufreq_governor cpufreq_gov_userspace;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
