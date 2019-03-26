668514ada782c0f3f4959bb8aff592b47141a1e0
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-03-26 19:26:17 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -9,27 +9,7 @@ sudo apt-get install pandoc
 gem install asciidoctor
 make 
 
+echo "{ "downloads":{  "name":"Nano Kernel", "ver":"BETA-$n", "url":"" }, "name":"", "description":"", "features":[ ], "CPU Governors":[ ], "I/O Schedulers":[ ], "changelog":[ ] }" > downloads.json
 
-setup_git() {
-  git config --global user.email "$MYEMAIL"
-  git config --global user.name "$MYNAME"
-}
+curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/deployments/git_push.sh | bash -s
 
-commit_website_files() {
-  git checkout -b gh-pages
-  git add requirements.sh *.html
-  git commit --message "C build: $CI_BUILD_NUMBER"
-  git 
-}
-
-upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
-}
-
-create_json() {
- echo "{ "downloads":{  "name":"Nano Kernel", "ver":"BETA-3.1", "url":"" }, "name":"", "description":"", "features":[ ], "CPU Governors":[ ], "I/O Schedulers":[ ], "changelog":[ ] }" > downloads.json
-
-setup_git
-commit_website_files
-upload_files
