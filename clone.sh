0d1206e617e1fe0d1ba88be925bd607c470ef0ce
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-03-29 08:40:51 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-git clone --depth=1 --no-single-branch https://gitlab.com/pshreejoy15/nano nano
+git clone --depth=1 --no-single-branch https://gitlab.com/pshreejoy15/nano /home/rof/src/github.com/shreejoy/Nano_rosy/nano
