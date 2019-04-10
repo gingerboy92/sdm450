f07b4d0fe924662b11190c5a4bfacc7905c964b7
Syukri Lajin <ginger_boy92@hotmail.com>
2019-04-11 02:08:53 +0800

    random: remove guard

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1900,7 +1900,6 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 	if (flags & GRND_RANDOM)
 		return _random_read(flags & GRND_NONBLOCK, buf, count);
 
-#ifdef CONFIG_GETRANDOM_BLOCKING_INIT
 	if (!crng_ready()) {
 		if (flags & GRND_NONBLOCK)
 			return -EAGAIN;
@@ -1908,7 +1907,6 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 		if (signal_pending(current))
 			return -ERESTARTSYS;
 	}
-#endif
 	return urandom_read(NULL, buf, count, NULL);
 }
 
